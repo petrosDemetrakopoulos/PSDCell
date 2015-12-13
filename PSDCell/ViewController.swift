9ef1ba3e529dfbbb269bf7df64d396489fb3bb97
Petros Demetrakopoulos <Petros@petross-macbook-pro.local>
2015-12-13 20:56:59 +0200

    Initial Commit

---
+++ PSDCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PSDCell
+//
+//  Created by Petros Demetrakopoulos on 13/12/15.
+//  Copyright © 2015 Petros Demetrakopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
